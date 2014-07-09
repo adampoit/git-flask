9da35b079346f9cf533df77cee7521bbe0ffad36
unknown <metalkeys404@gmail.com>
2014-07-09 02:32:05 -0700

    Add touch functionality.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import git
+import os
 
 app = Flask(__name__)
 
@@ -7,9 +8,15 @@ repos = { "git-flask": "/home/adampoit/webapps/git_flask/git_flask" }
 
 @app.route("/", methods=["POST"])
 def gitpull():
-	repo = git.Repo(repos[request.get_json()["repository"]["name"]])
+	path = repos[request.get_json()["repository"]["name"]]
+	repo = git.Repo(path)
 	repo.git.pull()
+	touch(os.path.dirname(path) + "/wsgi.py")
 	return "", 204
+	
+def touch(fname, times=None):
+    with open(fname, 'a'):
+        os.utime(fname, times)
 
 if __name__ == "__main__":
 	app.run()
